24fba72b45bc38d1c043cd692cee30fddbe32c29
Jiashuo Tong <jtong8@illinois.edu>
2020-06-12 04:07:25 -0500

    Improved class structure

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
+function App() {
+  return (
+    <div id='wrap'>
+      <Navi />
+      <Calc />
+    </div>
+  );
+}
+
 class Navi extends React.Component {
   render () {
     return (
@@ -31,113 +40,213 @@ class Navi extends React.Component {
   }
 }
 
-function Switch (props) {
-  const selected = props.selected;
-  if (selected === "KM") {
-    return (<Table />)
-  } else if (selected === "GM") {
-    return <h1> I'm GM </h1>;
-  } else if (selected === "LR") {
-    return <h1> I'm LR </h1>;
-  } else if (selected === "LogR") {
-    return <h1> I'm LogR </h1>;
-  } else if (selected === "BN") {
-    return <h1> I'm BN </h1>;
-  } else if (selected === "HM") {
-    return <h1> I'm HM </h1>;
-  } else if (selected === "FG") {
-    return <h1> I'm FG </h1>;
-  }
-  return <h1> I'm Nothing </h1>;
-}
-
-const styles = {
-  cardStyle: {
-      height: "95%"
-  }
-}
-
-class Table extends React.Component {
-  constructor (props) {
+class Calc extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {type: "point", number: 0};
-    this.addRow = this.addRow.bind(this);
+    this.state = {selected:'KM', data: []};
+    this.addPoint = this.addPoint.bind(this);
+    this.deletePoint = this.deletePoint.bind(this);
+    this.selectModule = this.selectModule.bind(this);
+
   }
 
-  addRow () {
-    
+  selectModule (mod) {
+    this.setState({selected: mod});
+  }
+
+  addPoint (inputX, inputY) {
+    this.setState({data: [[parseFloat(inputX), parseFloat(inputY)]].concat(this.state.data)});
+  }
+
+  deletePoint (id) {
+    var newData = [];
+    for (var i=0; i<this.state.data.length; i++) {
+      if (i !== parseInt(id)) {
+        newData.push(this.state.data[i]);
+      }
+    }
+
+    this.setState({data: newData});
   }
 
   render () {
-
+    var selected = this.state.selected;
+    var data = this.state.data;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>X</th>
-            <th>Y</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr id="addRow">
-            <td className="col-xs-3">
-              <input className="form-control addX" type="text" placeholder="Enter X" />
-            </td>
-            <td className="col-xs-3">
-              <input className="form-control addY" type="text" placeholder="Enter Y" />
-            </td>
-            <td className="col-xs-1 text-center">
-              <span className="addBtn">
-                <FontAwesomeIcon icon={faPlus} onClick={this.addRow}/>
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div id='calc'>
+        <div className='column left'>
+          <Sidebar selected={selected} data={data} selectModule={this.selectModule} addPoint={this.addPoint} deletePoint={this.deletePoint} />
+        </div>
+        <div className='column right'>
+          <Canvas selected={selected} data={data} />
+        </div>
+      </div>
     );
   }
 }
 
 
-class Toolbar extends React.Component {
-
+class Sidebar extends React.Component {
   constructor (props) {
     super(props);
     this.state = {selected: "KM"};
   }
 
   render () {
-    const selected = this.state.selected;
+    const selected = this.props.selected;
+    var tools = undefined;
+    if (selected === "KM") {
+      tools = <KMTools data={this.props.data} addPoint={this.props.addPoint} deletePoint={this.props.deletePoint}/>;
+    } else if (selected === "GM") {
+      tools = <h1> I'm GM </h1>;
+    } else if (selected === "LR") {
+      tools = <h1> I'm LR </h1>;
+    } else if (selected === "LogR") {
+      tools = <h1> I'm LogR </h1>;
+    } else if (selected === "BN") {
+      tools = <h1> I'm BN </h1>;
+    } else if (selected === "HM") {
+      tools = <h1> I'm HM </h1>;
+    } else if (selected === "FG") {
+      tools = <h1> I'm FG </h1>;
+    } else {
+      tools = <h1> I'm Nothing </h1>;
+    }
+
     return (
-      <Card style={styles.cardStyle} className="rounded-0">
+      <Card className="sidebar rounded-0">
         <Card.Header>
           <Nav variant="tabs">
             <NavDropdown title="Clustering" id="nav-dropdown">
-              <NavDropdown.Item eventKey="c.1" onClick={()=>this.setState({selected: "KM"})}>K-Means</NavDropdown.Item>
-              <NavDropdown.Item eventKey="c.2" onClick={()=>this.setState({selected: "GM"})}>Gaussian Mixture</NavDropdown.Item>
+              <NavDropdown.Item eventKey="c.1" onClick={()=>this.props.selectModule("KM")}>K-Means</NavDropdown.Item>
+              <NavDropdown.Item eventKey="c.2" onClick={()=>this.props.selectModule("GM")}>Gaussian Mixture</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Regression" id="nav-dropdown">
-              <NavDropdown.Item eventKey="r.1" onClick={()=>this.setState({selected: "LR"})}>Linear Regression</NavDropdown.Item>
-              <NavDropdown.Item eventKey="r.2" onClick={()=>this.setState({selected: "LogR"})}>Logistic Regression</NavDropdown.Item>
+              <NavDropdown.Item eventKey="r.1" onClick={()=>this.props.selectModule("LR")}>Linear Regression</NavDropdown.Item>
+              <NavDropdown.Item eventKey="r.2" onClick={()=>this.props.selectModule("LogR")}>Logistic Regression</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Graph" id="nav-dropdown">
-              <NavDropdown.Item eventKey="g.1" onClick={()=>this.setState({selected: "BN"})}>Bayesian Network</NavDropdown.Item>
-              <NavDropdown.Item eventKey="g.2" onClick={()=>this.setState({selected: "HM"})}>Hidden Markov</NavDropdown.Item>
-              <NavDropdown.Item eventKey="g.3" onClick={()=>this.setState({selected: "FG"})}>Factor Graph</NavDropdown.Item>
+              <NavDropdown.Item eventKey="g.1" onClick={()=>this.props.selectModule("BN")}>Bayesian Network</NavDropdown.Item>
+              <NavDropdown.Item eventKey="g.2" onClick={()=>this.props.selectModule("HM")}>Hidden Markov</NavDropdown.Item>
+              <NavDropdown.Item eventKey="g.3" onClick={()=>this.props.selectModule("FG")}>Factor Graph</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Card.Header>
         <Card.Body>
-          <Switch selected={selected}/>
+          {tools}
         </Card.Body>
       </Card>
     );
   }
 }
 
+function KMTools(props) {
+  return (
+    <div id='KMTools'>
+      <Table data={props.data} addPoint={props.addPoint} deletePoint={props.deletePoint}/>
+    </div>
+  );
+}
+
+class Table extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {inputX: '', inputY: ''};
+    this.editX = this.editX.bind(this);
+    this.editY = this.editY.bind(this);
+  }
+
+  editX (event) {
+    this.setState({inputX: event.target.value});
+  }
+
+  editY (event) {
+    this.setState({inputY: event.target.value});
+  }
+
+  render () {
+    var list = [];
+    for (var i=0; i<this.props.data.length; i++){
+      list.push(
+        <tr>
+          <td className="col-xs-3">
+            <input className="form-control border-0" type="text" value={this.props.data[i][0]} />
+          </td>
+          <td className="col-xs-3">
+            <input className="form-control border-0" type="text" value={this.props.data[i][1]} />
+          </td>
+          <td className="col-xs-1 text-center">
+            <span className="delBtn">
+              <FontAwesomeIcon id={i} icon={faMinus} onClick={e=>this.props.deletePoint(e.target.id)}/>
+            </span>
+          </td>
+        </tr>
+      );
+    }
+    
+    return (
+      <div className = "table-container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>X</th>
+              <th>Y</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="col-xs-3">
+                <input className="form-control addX" name="inputX" type="text" placeholder="Enter X" onChange={this.editX}/>
+              </td>
+              <td className="col-xs-3">
+                <input className="form-control addY" name="inputY" type="text" placeholder="Enter Y" onChange={this.editY}/>
+              </td>
+              <td className="col-xs-1 text-center">
+                <span >
+                  <FontAwesomeIcon className="addBtn" icon={faPlus} onClick={()=>this.props.addPoint(this.state.inputX, this.state.inputY)}/>
+                </span>
+              </td>
+            </tr>
+            {list}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class Canvas extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {selected: "KM"};
+  }
+
+  render () {
+    const selected = this.props.selected;
+    if (selected === "KM") {
+      return <XYcoord />;
+    } else if (selected === "GM") {
+      return <XYcoord />;
+    } else if (selected === "LR") {
+      return <XYcoord />;
+    } else if (selected === "LogR") {
+      return <XYcoord />;
+    } else if (selected === "BN") {
+      return <h1> Fill me with BN </h1>;
+    } else if (selected === "HM") {
+      return <h1> Fill me with HM </h1>;
+    } else if (selected === "FG") {
+      return <h1> Fill me with FG </h1>;
+    } else {
+      return <h1> Fill me with Nothing </h1>;
+    }
+  }
+}
+
+
+
 class XYcoord extends React.Component {
-  
   constructor(props) {
     super(props);
     // Don't call this.setState() here!
@@ -284,11 +393,10 @@ class XYcoord extends React.Component {
   }
 
   render () {
-    console.log(this.state.W, this.state.H);
     var grids = [];
-    var xaxis = <line id='x-axis' x1='0' y1={this.state.cY} x2={this.state.W} y2={this.state.cY} strokeWidth='1' stroke='black'/>;
-    var yaxis = <line id='y-axis' x1={this.state.cX} y1='0' x2={this.state.cX} y2={this.state.H} strokeWidth='1' stroke='black'/>;
-    var otick = <text className='tick' x={this.state.cX-3} y={this.state.cY+12} fill='black' fontSize='10pt' textAnchor='end' fontFamily="math">{0}</text>
+    var xaxis = <line key='xa' id='x-axis' x1='0' y1={this.state.cY} x2={this.state.W} y2={this.state.cY} strokeWidth='1' stroke='black'/>;
+    var yaxis = <line key='ya' id='y-axis' x1={this.state.cX} y1='0' x2={this.state.cX} y2={this.state.H} strokeWidth='1' stroke='black'/>;
+    var otick = <text key='ot' className='tick' x={this.state.cX-3} y={this.state.cY+12} fill='black' fontSize='10pt' textAnchor='end' fontFamily="math">{0}</text>
 
     grids.push(xaxis);
     grids.push(yaxis);
@@ -300,20 +408,19 @@ class XYcoord extends React.Component {
     var yr = Math.floor((this.state.W-this.state.cX)/this.state.gS);
 
     for (var i=xu; i<=xd; i++) {
-      console.log(i)
       var y = this.state.cY + i*this.state.gS;
       if (i % this.state.nS === 0) {
-        grids.push(<line className='x-grids' x1='0' y1={y} x2={this.state.W} y2={y} strokeWidth='0.5' stroke='#666666'/>);
+        grids.push(<line key={'hl'+i} className='x-grids' x1='0' y1={y} x2={this.state.W} y2={y} strokeWidth='0.5' stroke='#666666'/>);
         if (i !== 0) {
           var yval = this.state.co*Math.pow(10,this.state.exp)*(-i/this.state.nS);
           if (Math.abs(this.state.exp) >= 6) {
               yval = yval.toExponential();
           }
-          grids.push(<text className='tick' x={this.state.cX-3} y={y+5} fill='black' fontSize='10pt' textAnchor='end' fontFamily="math">{yval}</text>);
+          grids.push(<text key={'ht'+i} className='tick' x={this.state.cX-3} y={y+5} fill='black' fontSize='10pt' textAnchor='end' fontFamily="math">{yval}</text>);
         }
       }
       else {
-        grids.push(<line className='x-grids' x1='0' y1={y} x2={this.state.W} y2={y} strokeWidth='0.5' stroke='#CCCCCC'/>);
+        grids.push(<line key={'hl'+i} className='x-grids' x1='0' y1={y} x2={this.state.W} y2={y} strokeWidth='0.5' stroke='#CCCCCC'/>);
       }
     }
 
@@ -321,22 +428,22 @@ class XYcoord extends React.Component {
       var x = this.state.cX + i*this.state.gS;
       
       if (i % this.state.nS === 0) {
-        grids.push(<line className='x-grids' x1={x} y1='0' x2={x} y2={this.state.H} strokeWidth='0.5' stroke='#666666'/>);
+        grids.push(<line key={'vl'+i} className='x-grids' x1={x} y1='0' x2={x} y2={this.state.H} strokeWidth='0.5' stroke='#666666'/>);
         if (i !== 0) {
           var xval = this.state.co*Math.pow(10,this.state.exp)*(i/this.state.nS);
           if (Math.abs(this.state.exp) >= 6) {
               xval = xval.toExponential();
           }
-          grids.push(<text className='tick' x={x} y={this.state.cY+12} fill='black' fontSize='10pt' textAnchor='middle' fontFamily="math">{xval}</text>);
+          grids.push(<text key={'vt'+i} className='tick' x={x} y={this.state.cY+12} fill='black' fontSize='10pt' textAnchor='middle' fontFamily="math">{xval}</text>);
         }
       }
       else {
-        grids.push(<line className='x-grids' x1={x} y1='0' x2={x} y2={this.state.H} strokeWidth='0.5' stroke='#CCCCCC'/>);
+        grids.push(<line key={'vl'+i} className='x-grids' x1={x} y1='0' x2={x} y2={this.state.H} strokeWidth='0.5' stroke='#CCCCCC'/>);
       }
     }
 
-    var coordsys = <svg width='100%' height='100%' left='50%' className='coordsys' onWheel={this.scrollZoom} onMouseMove={this.mouseMove} onMouseUp={this.mouseUp} onMouseDown={this.mouseDown}>
-                    <g id='b' width='100%' height='100%'>
+    var coordsys = <svg className='coordsys' onWheel={this.scrollZoom} onMouseMove={this.mouseMove} onMouseUp={this.mouseUp} onMouseDown={this.mouseDown}>
+                    <g id='b'>
                       {grids}
                     </g>
                   </svg>
@@ -347,27 +454,50 @@ class XYcoord extends React.Component {
   }
 }
 
-function Calc () {
-  return (
-    <div id='calc'>
-      <div className='column left'>
-        <Toolbar />
-      </div>
-      <div className='column right'> 
-        <XYcoord />
-      </div>
-    </div>
-  );
+class Points extends React.Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {type: 'points'}
+  }
+
+  
 }
 
-function App() {
-  return (
-    <div id='wrap'>
-      <Navi />
-      <Calc />
-    </div>
-  );
-}
+/*
+App
+  Nav
+  Calc
+    Sidebar
+      KMtools
+        Table
+        Table
+        Toolbar
+      GMtools
+        Table
+        Table
+        Toolbar
+    
+    Canvas
+      KMvis
+        XYcoord
+        Points
+      GMvis
+        XYcoord
+        Points
+      HMvis
+        Graphsys
+      FGvis
+        Graphsys
+
+    Engine
+      KMengine
+      GMengine
+      
+      
+*/
+
+
 
 ReactDOM.render(
   <App />,
--- src/save.js
+++ src/save.js
@@ -1,76 +1,19 @@
-redraw () {
-    var xu = -Math.floor(cY/gS);
-    var xd = Math.floor((H-cY)/gS);
-    var yl = -Math.floor(cX/gS);
-    var yr = Math.floor((W-cX)/gS);
-    
-    var lines = '';
-    var ticks = '';
-
-    for (i=xu; i<=xd; i++) {
-        var y = cY + i*gS;
-        if (i % nS == 0){
-            var line = `<line class='x-grids' x1='0' y1=${y} x2=${W} y2=${y} stroke-width='0.5' stroke='#666666'/>`;
-            if (i != 0) {
-                var yval = co*Math.pow(10,exp)*(-i/nS);
-                if (Math.abs(exp) >= 6){
-                    yval = yval.toExponential();
-                }
-                var ytick = `<text x=${cX-3} y=${y+3} fill='black' font-size='6pt' text-anchor='end' font-family="math">${yval}</text>`
-            }
-        }
-        else {
-            var line = `<line class='x-grids' x1='0' y1=${y} x2=${W} y2=${y} stroke-width='0.5' stroke='#CCCCCC'/>`;
-        }
-        lines += line;
-        ticks += ytick;
+function Switch (props) {
+    const selected = props.selected;
+    if (selected === "KM") {
+      return <KMTools />;
+    } else if (selected === "GM") {
+      return <h1> I'm GM </h1>;
+    } else if (selected === "LR") {
+      return <h1> I'm LR </h1>;
+    } else if (selected === "LogR") {
+      return <h1> I'm LogR </h1>;
+    } else if (selected === "BN") {
+      return <h1> I'm BN </h1>;
+    } else if (selected === "HM") {
+      return <h1> I'm HM </h1>;
+    } else if (selected === "FG") {
+      return <h1> I'm FG </h1>;
     }
-
-    for (i=yl; i<=yr; i++) {
-        var x = cX + i*gS;
-        if (i % nS == 0){
-            var line = `<line class='x-grids' x1=${x} y1=0 x2=${x} y2=${H} stroke-width='0.5' stroke='#666666'/>`;
-            if (i != 0) {
-                var xval = co*Math.pow(10,exp)*(i/nS);
-                if (Math.abs(exp) >= 6){
-                    xval = xval.toExponential();
-                }
-                var xtick = `<text x=${x} y=${cY+8} fill='black' font-size='6pt' text-anchor='middle' font-family="math">${xval}</text>`
-            }
-        }
-        else {
-            var line = `<line class='x-grids' x1=${x} y1=0 x2=${x} y2=${H} stroke-width='0.5' stroke='#CCCCCC'/>`;
-        }
-        lines += line;
-        ticks += xtick;
-    }
-
-    var xaxis = `<line id='x-axis' x1='0' y1=${cY} x2=${W} y2=${cY} stroke-width='1' stroke='black'/>`;
-    var yaxis = `<line id='y-axis' x1=${cX} y1=0 x2=${cX} y2=${H} stroke-width='1' stroke='black'/>`;
-    var otick = `<text x=${cX-3} y=${cY+8} fill='black' font-size='6pt' text-anchor='end' font-family="math">${0}</text>`
-    
-    lines += xaxis+yaxis;
-    ticks += otick;
-    grids.innerHTML = lines+ticks;
+    return <h1> I'm Nothing </h1>;
   }
-
-
-
-  for (var i=this.state.yl; i<=this.state.yr; i++) {
-    var x = this.state.cX + i*this.state.gS;
-    if (i % this.state.nS == 0){
-        var line = <line class='x-grids' x1={this.state.x} y1=0 x2={this.state.x} y2={this.state.H} stroke-width='0.5' stroke='#666666'/>;
-        if (i != 0) {
-            var xval = co*Math.pow(10,this.state.exp)*(i/this.state.nS);
-            if (Math.abs(this.state.exp) >= 6){
-                xval = xval.toExponential();
-            }
-            var xtick = <text x={this.state.x} y={this.state.cY+8} fill='black' font-size='6pt' text-anchor='middle' font-family="math">{xval}</text>;
-        }
-    }
-    else {
-        var line = <line class='x-grids' x1={this.state.x} y1=0 x2={this.state.x} y2={this.state.H} stroke-width='0.5' stroke='#CCCCCC'/>;
-    }
-    lines += line;
-    ticks += xtick;
-}
